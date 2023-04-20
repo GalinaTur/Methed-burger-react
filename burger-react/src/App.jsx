d1a940ffc60e09148fe93875057747cd46823228
Halyna Turajeva <gal227795@gmail.com>
2023-04-20 20:56:38 +0300

    created React project, added Header component

---
+++ burger-react/src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header/Header"
+
+export const App = () => (
+  <>
+    <Header/>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+  </>)
+
+
---
+++ burger-react/src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import { Container } from "../Container/Container";
+import styles from "./Header.module.scss";
+import classNames from "classnames";
+import logoSvg from "../../assets/img/sprite.svg"
+
+export const Header = () => (
+  <header className={styles.header}>
+    <Container className={styles.header__container}>
+    <svg className={styles.logo} viewBox="0 0 199 44" role="img" aria-roledescription="Логотип YourMeal">
+                <use href={`${logoSvg}#logo`}/>
+            </svg>
+
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>
+          Только самые{" "}
+          <span className={classNames(styles.title, styles.title_red)}>
+            сочные бургеры!
+          </span>
+        </h1>
+        <p className={styles.subtitle}>
+          Бесплатная доставка от 599<span className="currency">₴</span>
+        </p>
+      </div>
+    </Container>
+  </header>
+);
